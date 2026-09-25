6ae4ef2a9763758c7e740e0ea8adb66f2f9b0414
Florian Burger <florian_burger90@web.de>
2026-09-25 12:36:48 +0200

    Add files via upload

---
+++ easyvid.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+#webm-to-mkv-converter v0.1
+#Creator: Florian Burger
+#Date: 2026-09-22
+
+echo "o++++++++++++++++++++++++++++++++++++++++++++++o"
+echo "+      Welcome to easyvid-converter        +"
+echo "o++++++++++++++++++++++++++++++++++++++++++++++o"
+
+echo "Package ffmpeg is needed.. Checking if package needs to be installed..."
+echo "Please wait..."
+sudo apt update > /dev/null && sudo apt install ffmpeg > /dev/null
+echo "Done!"
+
+USER=$(whoami)
+mkdir /home/$USER/Videos/easyvid
+
+cd /home/$USER/Videos/easyvid
+
+DIRECTORY=$(pwd)
+echo "Currant used directory is $DIRECTORY!"
+
+read -r -p "Select INPUT-File:" INPUT
+read -r -p "Select OUTPUT-File:" OUTPUT
+
+ffmpeg -i $INPUT -c copy $DIRECTORY/$OUTPUT > /dev/null
